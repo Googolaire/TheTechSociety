943b4817b3f208dee6e89563ecf969a64226ceb1
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-04-25 19:19:57 +0500

    Update bounty.1.js

--- bounty.1.js
+++ bounty.1.js
@@ -1,1 +1,31 @@
+function most_cccurring_numbers(items)
+{
+	//Default variables.
+	var rVal = 1, items , count = i = 0, j;
+	items.push('');
+	//Loop as row and col.
+	for (i; i < items.length; i++) {
+		for (j = i; j < items.length; j++) {
+			console.log('i ' + items[i]);
+			console.log('j ' + items[j]);
+			//Determine either i == j?
+			if (items[i] == items[j]) {
+				count++;
+			} else if (rVal < count) {
+				rVal = count;
+				item = items[i];
+			}
+		}
+		count = 0;
+	}
+	return {
+		'item': item,
+		'count': rVal,
+	};
+}
 
+
+//Example one
+//console.log(most_cccurring_numbers([1, 3, 3,4]));
+//Example two
+//console.log(most_cccurring_numbers([1, 2, 3, 4, 3]));
