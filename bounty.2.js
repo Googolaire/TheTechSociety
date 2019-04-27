4b7500adfd160099bcb51c1fb7d15a2e556afc92
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-04-27 10:37:40 +0500

    Update bounty.2.js

--- bounty.2.js
+++ bounty.2.js
@@ -1,1 +1,48 @@
+function multiple_of_number(n, multiple)
+{
+	var rN = [];
+	for (let i = 1; i <= n; i++) {
+		if (i % multiple == 0) {
+			rN.push(i);
+		}
+	}
 
+	return rN;
+}
+function fizz(n)
+{
+	var rN = [], x;
+	x = multiple_of_number(n, 3);
+	for (let i = 0; i < x.length ; i++) {
+		rN.push("fizz");
+	}
+
+	return rN;
+}
+function buzz(n)
+{
+	var rN = [], x;
+	x = multiple_of_number(n, 5);
+	for (let i = 0; i < x.length ; i++) {
+		console.log('fizz');
+		rN.push("buzz");
+	}
+
+	return rN;
+}
+function fizz_buzz(n)
+{
+	return {
+		'fizz': fizz(n),
+		'buzz': buzz(n)		
+	};
+}
+
+//Multiple of number example
+console.log(multiple_of_number(500,7));
+//Fizz example
+console.log(fizz(50));
+//Buzz Example
+console.log(buzz(50));
+//FizzBuzz example
+console.log(fizz_buzz(50));
